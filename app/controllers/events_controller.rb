771657ba14ada5a167fce8a8f481c3b4cffa1b63
Alex Ung <alexung.ucsb@gmail.com>
2015-01-25 14:42:00 -0800

    Meetup.com api

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+  include ApplicationHelper
+
+  def index
+    @meetups = Event.all
+  end
+
+  def new
+    @meetup = Event.new
+  end
+
+  def create
+    @meetup = Event.new(event_params)
+    @meetup.save
+      meetup_api(ENV['MEETUP_API_KEY'], params[:zipcode])
+      redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :zipcode)
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def meetup_api(meetup_key, zipcode)
+    `curl 'https://api.meetup.com/2/events?member_id=108788592&key=' + ENV['MEETUP_API_KEY']`
+  end
+
 end
