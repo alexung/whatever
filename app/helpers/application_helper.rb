f203c1add6965d09f4476ddb15a2e79039de33ea
Alex Ung <alexung.ucsb@gmail.com>
2015-01-26 16:37:37 -0800

    Deleted unnecessary lines of code in the meetup api helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,6 @@ module ApplicationHelper
 
     meetup_api = MeetupApi.new
     @events = meetup_api.open_events(params)
-    #binding.pry
-    #unparsed_events = URI.encode(arg_for_uri_encode)
-    #@events = URI.parse(unparsed_events)
     @events['results'].each_with_index do |event, i|
       @event = Event.new(title: @events['results'][i]['name'], description: @events['results'][i]['description'], group_title: @events['results'][i]['group']['name'])#, city: (@events['results'][i]['venue']['city'] || nil), country: (@events['results'][i]['venue']['country']))
       @event.save
